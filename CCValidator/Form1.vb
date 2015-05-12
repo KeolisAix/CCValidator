62afefea522ecbd27eb6ccdab9ea63ece83d945b
Patrice Maldi <patrice.maldi@keolis.com>
2015-05-12 10:00:24 +0200

    Maj pour les 10 de chaque mois

--- CCValidator/Form1.vb
+++ CCValidator/Form1.vb
@@ -4,7 +4,7 @@
     ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     ' VARIABLES DE DEFINITION A ADAPTER
     ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    Dim Aujourdhui As DateTime = Date.Now.AddDays(-1)
+    Dim Aujourdhui As DateTime = Date.Now
     Dim Jour As String = Aujourdhui.ToString("dd")
     Dim Mois As String = Aujourdhui.ToString("MM")
     Dim Annee As String = Aujourdhui.ToString("yyyy")
@@ -17,28 +17,28 @@
     Public FICHIER_VIDAGE_FINAL As String = "C:\COMPTAGE\EVENEMENTS\CONTROLE\VIDAGE_CC_FINAL.csv"
     Public FICHIER_BUS As String = "C:\COMPTAGE\EVENEMENTS\CONTROLE\Bus.txt"
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        If Jour.ToString < 10 And Mois.Contains("0") = False Then
+        If Jour.ToString <= 10 And Mois.Contains("0") = False Then
             'MsgBox(Jour.Substring(1, 1))
             JourGlobal = Jour.Substring(1, 1)
             FICHIER_EVENEMENT_D1 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + Mois + "_" + JourGlobal + "_Evenement.txt"
             FICHIER_EVENEMENT_D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + Mois + "_" + JourGlobal + "_Evenement_D2.txt"
             FICHIER_EVENEMENT_D1D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + Mois + "_" + JourGlobal + "_Evenement_D1D2.txt"
         End If
-        If Jour.ToString < 10 And Mois.Contains("0") = True Then
+        If Jour.ToString <= 10 And Mois.Contains("0") = True Then
             JourGlobal = Jour.Substring(1, 1)
             MoisGlobal = Mois.Substring(1, 1)
             FICHIER_EVENEMENT_D1 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + JourGlobal + "_Evenement.txt"
             FICHIER_EVENEMENT_D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + JourGlobal + "_Evenement_D2.txt"
             FICHIER_EVENEMENT_D1D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + JourGlobal + "_Evenement_D1D2.txt"
         End If
-        If Jour.ToString > 10 And Mois.Contains("0") = True Then
+        If Jour.ToString >= 10 And Mois.Contains("0") = True Then
             JourGlobal = Jour.Substring(1, 1)
             MoisGlobal = Mois.Substring(1, 1)
             FICHIER_EVENEMENT_D1 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + Jour + "_Evenement.txt"
             FICHIER_EVENEMENT_D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + Jour + "_Evenement_D2.txt"
             FICHIER_EVENEMENT_D1D2 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + MoisGlobal + "_" + Jour + "_Evenement_D1D2.txt"
         End If
-        If Jour.ToString > 10 And Mois.Contains("0") = False Then
+        If Jour.ToString >= 10 And Mois.Contains("0") = False Then
             JourGlobal = Jour.Substring(1, 1)
             MoisGlobal = Mois.Substring(1, 1)
             FICHIER_EVENEMENT_D1 = "C:\COMPTAGE\EVENEMENTS\" + Annee + "_" + Mois + "_" + Jour + "_Evenement.txt"
@@ -48,11 +48,11 @@
         If My.Computer.FileSystem.FileExists(FICHIER_EVENEMENT_D1D2.ToString) Then
             My.Computer.FileSystem.DeleteFile(FICHIER_EVENEMENT_D1D2.ToString)
         End If
-        'MsgBox("/K CD C:\COMPTAGE\EVENEMENTS\ & TYPE " + FICHIER_EVENEMENT_D1 + " >> " + FICHIER_EVENEMENT_D1D2 + " & TYPE " + FICHIER_EVENEMENT_D2 + " >> " + FICHIER_EVENEMENT_D1D2 + " & move " + FICHIER_EVENEMENT_D1D2 + " C:\COMPTAGE\EVENEMENTS\CONTROLE\   & Exit")
+        ' MsgBox("/K CD C:\COMPTAGE\EVENEMENTS\ & TYPE " + FICHIER_EVENEMENT_D1 + " >> " + FICHIER_EVENEMENT_D1D2 + " & TYPE " + FICHIER_EVENEMENT_D2 + " >> " + FICHIER_EVENEMENT_D1D2 + " & move " + FICHIER_EVENEMENT_D1D2 + " C:\COMPTAGE\EVENEMENTS\CONTROLE\   & Exit")
         'MsgBox(Aujourdhui.ToString("d/MM/yyyy"))
         Process.Start("CMD.exe", "/K CD C:\COMPTAGE\EVENEMENTS\ & TYPE " + FICHIER_EVENEMENT_D1 + " >> " + FICHIER_EVENEMENT_D1D2 + " & TYPE " + FICHIER_EVENEMENT_D2 + " >> " + FICHIER_EVENEMENT_D1D2 + " & Exit")
         LireEvenement(FICHIER_EVENEMENT_D1D2, "le PC a reçu l'ACK de mémoire effacée du bus ") ' + ECRIRE_BUS
-        AjouterValeur(FICHIER_BUS, FICHIER_VIDAGE_ORIGNE)
+        AjouterValeur(FICHIER_BUS, FICHIER_VIDAGE_FINAL)
         Process.Start("CMD.exe", "/K move " + FICHIER_EVENEMENT_D1D2 + " C:\COMPTAGE\EVENEMENTS\CONTROLE\ & Exit")
         ExportToCSV(FICHIER_VIDAGE_FINAL)
     End Sub
